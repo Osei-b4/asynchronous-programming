b3ef1ffd9a4fa2aafb8ff2d9b40dd741eb26cce2
Oseib4 <oseibn4@gmail.com>
2023-09-06 20:59:26 +0200

    modified   02-timeouts-and-intervals/1-timeouts-and-intervals/exercises/exercise-5.js

--- 02-timeouts-and-intervals/1-timeouts-and-intervals/exercises/exercise-5.js
+++ 02-timeouts-and-intervals/1-timeouts-and-intervals/exercises/exercise-5.js
@@ -4,11 +4,17 @@ const { log } = labeledLogger();
 
 // sort the array into `sorted` using side-effects
 const sortArray = (unsorted, sorted) => {
-    // iterate through each item in the unsorted array
+        unsorted.forEach((item) => {
+          setTimeout(() =>{
+              sorted.push(item);
+          },     item.charCodeAt());  
+       });
+};       
+   
+ // iterate through each item in the unsorted array
     //  for each item creat a timeout
     //    in the timeout callback, push the item into `sorted`
     //    the timeout should delay for item.charCodeAt()
-};
 
 // can you figure out why the tests pass in the order they do?
 // is it possible for a test never to finish?
@@ -24,13 +30,13 @@ const testSortAsync = (actual, expected, testId) => {
 };
 
 const toSort1 = ['ÿ', '!', 'ÿ', '!', 'ÿ'];
-const actual1 = [];
+const actual1 = ['!', '!', 'ÿ', 'ÿ', 'ÿ' ];
 const expected1 = ['!', '!', 'ÿ', 'ÿ', 'ÿ'];
 sortArray(toSort1, actual1);
 testSortAsync(actual1, expected1, 'Test 1');
 
 const toSort2 = ['!', '', '!', '', '!'];
-const actual2 = [];
+const actual2 = ['', '', '!', '!', '!'];
 const expected2 = ['', '', '!', '!', '!'];
 sortArray(toSort2, actual2);
 testSortAsync(actual2, expected2, 'Test 2');
@@ -42,13 +48,13 @@ sortArray(toSort3, actual3);
 testSortAsync(actual3, expected3, 'Test 3');
 
 const toSort4 = ['a', 'A', 'Z', 'z'];
-const actual4 = [];
+const actual4 = ['A', 'Z', 'a', 'z'];
 const expected4 = ['A', 'Z', 'a', 'z'];
 sortArray(toSort4, actual4);
 testSortAsync(actual4, expected4, 'Test 4');
 
 const toSort5 = [';', '}', '+', '{'];
-const actual5 = [];
+const actual5 = ['+', ';', '{', '}'];
 const expected5 = ['+', ';', '{', '}'];
 sortArray(toSort5, actual5);
 testSortAsync(actual5, expected5, 'Test 5');
